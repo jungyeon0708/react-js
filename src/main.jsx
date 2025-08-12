51fa717af6f7b23411a4888d82bb3c4b7f0c6534
yamoo9 <yamoo9@naver.com>
2025-08-13 04:02:49 +0900

    config. 데모 업데이트
    - 스타일 변경
    - 단축키 정보 추가
    - 리액트 100% 적용
    - jsconfig.json 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,8 @@
+import { createRoot } from 'react-dom/client'
 import './styles/main.css'
-import './demo'
+import App from './app'
+
+const container = document.getElementById('container')
+if (!container) throw new Error('문서에 #container 요소가 존재하지 않습니다.')
+
+createRoot(container).render(<App />)
